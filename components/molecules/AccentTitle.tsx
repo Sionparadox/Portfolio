e0967d25adc3aec3ac19f85c64b52c09fd4b5fe1
Sionparadox <sion9999@naver.com>
2026-01-15 16:31:15 +0900

    [fix] AccentTitle 범용 컴포넌트로 사용할 수 있도록 수정

--- components/molecules/AccentTitle.tsx
+++ components/molecules/AccentTitle.tsx
@@ -1,30 +1,47 @@
 import { cn } from '@/lib/utils';
 
 type AccentTitleProps = {
+  as?: 'h1' | 'h2' | 'h3';
   text?: string;
   accentText?: string;
+  text2?: string;
+  color?: 'primary' | 'neon';
   description?: string;
   className?: string;
 };
+
+const colorClasses = {
+  primary: 'from-secondary to-primary bg-linear-to-tr ',
+  neon: 'bg-linear-to-tr from-cyan-500 to-purple-500 ',
+};
+const sizeClasses = {
+  h1: 'text-3xl font-black md:text-5xl',
+  h2: 'text-2xl font-bold md:text-4xl',
+  h3: 'text-xl font-semibold md:text-3xl',
+};
+
 const AccentTitle = ({
+  as: Component = 'h1',
   text = '',
   accentText = '',
+  text2 = '',
+  color = 'primary',
   description = '',
   className = '',
 }: AccentTitleProps) => {
   return (
     <div>
-      <h1
-        className={cn(
-          'text-foreground text-3xl font-black md:text-5xl',
-          className
-        )}
+      <Component
+        className={cn('text-foreground', sizeClasses[Component], className)}
       >
         {text}{' '}
-        <span className='from-secondary to-primary bg-linear-to-tr bg-clip-text text-transparent'>
+        <span
+          className={cn(colorClasses[color], 'bg-clip-text text-transparent')}
+        >
           {accentText}
-        </span>
-      </h1>
+        </span>{' '}
+        {text2}
+      </Component>
       {description && (
         <p className='text-muted-foreground mt-2 text-lg leading-relaxed'>
           {description}
