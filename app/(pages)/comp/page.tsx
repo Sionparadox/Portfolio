a155ae1ca29a127150c9d684555417ea173f19c2
Sionparadox <sion9999@naver.com>
2026-01-23 16:44:35 +0900

    [fix] Comp페이지에 Todo 넣기

--- app/(pages)/comp/page.tsx
+++ app/(pages)/comp/page.tsx
@@ -1,45 +1,61 @@
 import Container from '@/components/atoms/Container';
-import ExploreCard from '@/components/molecules/ExploreCard';
-import ProjectListCard from '@/components/molecules/ProjectListCard';
-import { ProjectItemType } from '@/types/project';
-import { FaGithub } from 'react-icons/fa6';
+import AccentTitle from '@/components/molecules/AccentTitle';
 
-const tempProject: ProjectItemType = {
-  id: '1',
-  slug: 'project-1',
-  title: 'Project 1',
-  overview:
-    'Project 1 overview Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
-  description: 'Project 1 description',
-  icon: '/img/projects/one-hada/icon.png',
-  contributions: ['Contribution 1', 'Contribution 2', 'Contribution 3'],
-  insights: ['Insight 1', 'Insight 2', 'Insight 3'],
-  teamSize: 3,
-  thumbnail: '/img/projects/one-hada/thumbnail.png',
-  techStack: ['React', 'Next.js', 'TypeScript'],
-  category: 'Web',
-  startDate: new Date(),
-  endDate: new Date(),
-  featured: false,
-  order: 0,
-  createdAt: new Date(),
-  updatedAt: new Date(),
+const TodoList = ({
+  title,
+  contents,
+}: {
+  title: string;
+  contents: string[];
+}) => {
+  return (
+    <div className='bg-card flex w-full flex-col gap-2 rounded-xl border p-4'>
+      <h2 className='text-lg font-bold'>{title}</h2>
+      {contents.map((content) => (
+        <p key={content} className='text-card-foreground text-left'>
+          {content}
+        </p>
+      ))}
+    </div>
+  );
 };
+
 const Page = () => {
   return (
     <Container className='flex flex-col items-center justify-center gap-12 text-center'>
-      <div className='flex w-full justify-center gap-4 py-8 text-left'>
-        <ExploreCard
-          title='Explore Card'
-          description='Explore Card is a card component that is styled to look like a glass.'
-          icon={FaGithub}
-          link='https://github.com'
+      <AccentTitle accentText='TODO' className='sm:pt-8' />
+      <div className='grid w-full grid-cols-2 gap-2'>
+        <TodoList
+          title='메인'
+          contents={['Hero 변경', 'Explore 내용', '테마 토글버튼 변경']}
         />
-      </div>
-      <div className='flex w-full flex-col gap-2'>
-        <ProjectListCard project={tempProject} />
-        <ProjectListCard project={tempProject} />
-        <ProjectListCard project={tempProject} />
+        <TodoList
+          title='레이아웃'
+          contents={[
+            '모바일 드롭다운 메뉴 구림',
+            'Layout 조정(Container과 layout)',
+          ]}
+        />
+        <TodoList
+          title='About'
+          contents={[
+            'About 이미지 실제 사진으로',
+            'About 소개글 실제 내용으로',
+            'Timeline 630px 에서 반응형 깨짐',
+            '타임라인 스켈레톤',
+          ]}
+        />
+        <TodoList
+          title='Projects'
+          contents={['프로젝트 스켈레톤', '리스트용 프로젝트리스트카드 만들기']}
+        />
+        <TodoList
+          title='Contact'
+          contents={[
+            'Contact 제출버튼 애니메이션',
+            'ContactForm 인풋부분 라벨링 + 배경색 카드로',
+          ]}
+        />
       </div>
     </Container>
   );
