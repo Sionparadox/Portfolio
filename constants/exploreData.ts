9bd156525ef664fe2879a5f8b1eb9d7653cf9401
Sionparadox <sion9999@naver.com>
2026-02-10 18:20:38 +0900

    [update] Explore 임시데이터 수정

--- constants/exploreData.ts
+++ constants/exploreData.ts
@@ -12,25 +12,25 @@ export type ExploreItem = {
 export const exploreItems: ExploreItem[] = [
   {
     title: 'Home',
-    description: '홈스윗홈',
+    description: '저의 공간에 온 것을 환영합니다. 자유롭게 둘러보세요.',
     icon: FaHome,
     link: '/',
   },
   {
     title: 'About',
-    description: '어바웃타임',
+    description: '저의 가치관과 성장 과정을 따라가보세요.',
     icon: FaUser,
     link: '/about',
   },
   {
     title: 'Projects',
-    description: '프로젝트랄랄레로트랄랄라 프로젝트랄랄레로트랄랄라 ',
+    description: '문제를 해결하며 쌓아온 경험과 도전의 기록을 확인해보세요.',
     icon: FaFileCode,
     link: '/projects',
   },
   {
     title: 'Contact',
-    description: '연락해~',
+    description: '새로운 인연은 언제나 환영입니다. 당신의 이야기를 들려주세요.',
     icon: FaEnvelope,
     link: '/contact',
   },
