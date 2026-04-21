5a1baedbe4d2d0f61b238079a2043c8484c6c063
Sionparadox <sion9999@naver.com>
2026-04-21 18:17:49 +0900

    [feat] 관리자 로그인 페이지 추가

---
+++ app/admin/signin/page.tsx
@@ -0,0 +1,33 @@
+import { signIn } from '@/auth';
+import { Button } from '@/components/atoms/Button';
+import Container from '@/components/atoms/Container';
+import AccentTitle from '@/components/molecules/AccentTitle';
+import SignInButton from '@/components/molecules/SignInButton';
+import Link from 'next/link';
+
+const AdminSignInPage = () => {
+  return (
+    <Container className='flex grow flex-col items-center justify-center gap-8 text-center'>
+      <AccentTitle
+        text='Admin'
+        accentText='Login'
+        description='관리자만 접근 가능한 페이지입니다.'
+      />
+
+      <form
+        action={async () => {
+          'use server';
+          await signIn('github', { redirectTo: '/admin' });
+        }}
+        className='flex w-full max-w-md flex-col justify-center gap-2 sm:flex-row'
+      >
+        <SignInButton />
+        <Button asChild className='flex-1'>
+          <Link href='/'>메인으로 돌아가기</Link>
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default AdminSignInPage;
