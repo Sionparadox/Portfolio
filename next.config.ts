9a4c1fe602dda8524c538bfc54c1254b9f323a24
Sionparadox <sion9999@naver.com>
2026-04-30 18:54:11 +0900

    [fix] 로컬 폰트로 변경하여 네트워크 종속항목 트리 단순화

--- next.config.ts
+++ next.config.ts
@@ -17,16 +17,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config) {
-    // CssMinimizerPlugin 제거 - bg-conic 등 커스텀 CSS가 제거되는 것을 방지
-    config.optimization.minimizer = config.optimization.minimizer.filter(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (plugin: any) => {
-        return !plugin.constructor.name.includes('CssMinimizerPlugin');
-      }
-    );
-    return config;
-  },
+  // webpack(config) {
+  //   // CssMinimizerPlugin 제거 - bg-conic 등 커스텀 CSS가 제거되는 것을 방지
+  //   config.optimization.minimizer = config.optimization.minimizer.filter(
+  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //     (plugin: any) => {
+  //       return !plugin.constructor.name.includes('CssMinimizerPlugin');
+  //     }
+  //   );
+  //   return config;
+  // },
 };
 
 export default nextConfig;
